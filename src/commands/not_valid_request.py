884adb5211f4413e8041b20186a5f924967dfb1c
Arthur-king18 <arturik060406@gmail.com>
2023-10-30 20:53:02 +0400

    bug fix

--- src/commands/not_valid_request.py
+++ src/commands/not_valid_request.py
@@ -5,5 +5,5 @@ from aiogram import types
 
 @dp.message_handler()
 async def not_valid_request_handler(message: types.Message):
-    await bot.send_message(message.chat.id, 'Невалидный запос. Пример запроса: {"dt_from": "2022-09-01T00:00:00",'
+    await bot.send_message(message.chat.id, 'Невалидный запрос. Пример запроса: {"dt_from": "2022-09-01T00:00:00",'
                                             ' "dt_upto": "2022-12-31T23:59:00", "group_type": "month"}')
