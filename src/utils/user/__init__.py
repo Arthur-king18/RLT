396076c61e52ad47dd73f94fd2c3c5a33b9e163e
Arthur-king18 <arturik060406@gmail.com>
2023-10-30 20:42:25 +0400

    delete files

--- src/utils/user/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.utils.user.user_location import UserLocation
-from src.utils.user.user_service import UserService
-from src.utils.user.user_helper import UserHelper
-from src.utils.user.user_create_message import UserCreateMessage
