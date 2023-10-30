5a5d16db148cd57ddc831d2a05025d376da28781
Arthur-king18 <arturik060406@gmail.com>
2023-10-30 20:42:41 +0400

    change initialize bot

--- src/config/initialize.py
+++ src/config/initialize.py
@@ -3,8 +3,7 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from src.config import config
 
 from aiogram import Bot, Dispatcher
-from aiogram.types import ParseMode
 
-bot = Bot(config.BOT_API_TOKEN, parse_mode=ParseMode.MARKDOWN_V2)
+
+bot = Bot(config.BOT_API_TOKEN)
 dp = Dispatcher(bot)
-dp.middleware.setup(LoggingMiddleware())
